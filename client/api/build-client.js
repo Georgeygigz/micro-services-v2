5558b17193342dba0e60c8ed4de37137795b3ac8
georgeygigz <georgeymutti@gmail.com>
2024-02-28 13:12:57 +0300

    update ingress file

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -3,8 +3,8 @@ import axios from "axios";
 export default ({ req }) => {
   if (typeof window === "undefined") {
     return axios.create({
-      baseURL:
-        "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local",
+      // baseURL:"http://ingress-nginx-controller.ingress-nginx.svc.cluster.local",
+      baseURL:"http://ticketing-app-prod.store",
       headers: req.headers,
     });
   } else {
@@ -13,3 +13,5 @@ export default ({ req }) => {
     });
   }
 };
+
+
